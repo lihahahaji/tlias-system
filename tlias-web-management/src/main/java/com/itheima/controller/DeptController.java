08860553872986f6f80b6286e7d2ae07bdda4fc1
Haji <835510106@qq.com>
2024-04-19 20:13:22 +0800

    update deptController

--- tlias-web-management/src/main/java/com/itheima/controller/DeptController.java
+++ tlias-web-management/src/main/java/com/itheima/controller/DeptController.java
@@ -1,5 +1,11 @@
 package com.itheima.controller;
 
+import com.itheima.pojo.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -7,4 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class DeptController {
+    private static final Logger log = LoggerFactory.getLogger(DeptController.class);
+
+//    @RequestMapping(value = "/depts", method = RequestMethod.GET)
+    @GetMapping("/depts")
+    public Result list(){
+        log.info("查询部门数据");
+
+
+        return Result.success();
+    }
 }
