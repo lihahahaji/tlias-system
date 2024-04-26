92fdc9dbc23067405362dcc388bd6f05ecfc767f
Haji <835510106@qq.com>
2024-04-26 22:28:48 +0800

    完善登录接口，新增JWT登录校验

--- tlias-web-management/src/main/java/com/itheima/controller/LoginController.java
+++ tlias-web-management/src/main/java/com/itheima/controller/LoginController.java
@@ -4,11 +4,15 @@ package com.itheima.controller;
 import com.itheima.pojo.Emp;
 import com.itheima.pojo.Result;
 import com.itheima.service.EmpService;
+import com.itheima.utils.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -21,7 +25,17 @@ public class LoginController {
     public Result login(@RequestBody Emp emp)
     {
         Emp e = empService.login(emp);
-        return e!=null?Result.success():Result.error("用户名或者密码错误");
+
+        if(e != null){
+            Map<String,Object> claims = new HashMap<>();
+            claims.put("id",e.getId());
+            claims.put("username",e.getUsername());
+            claims.put("name",e.getName());
+
+            String jwt = JwtUtils.generateJwt(claims);
+            return Result.success(jwt);
+        }
+        return Result.error("用户名或者密码错误");
     }
 
 }
