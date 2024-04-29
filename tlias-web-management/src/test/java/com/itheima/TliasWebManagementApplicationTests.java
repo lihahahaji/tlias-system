0230922e3984e7d518b11b905990340007cc678d
Haji <835510106@qq.com>
2024-04-29 15:58:40 +0800

    测试bean 的获取

--- tlias-web-management/src/test/java/com/itheima/TliasWebManagementApplicationTests.java
+++ tlias-web-management/src/test/java/com/itheima/TliasWebManagementApplicationTests.java
@@ -1,17 +1,23 @@
 package com.itheima;
 
+import com.itheima.controller.DeptController;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-//@SpringBootTest
+@SpringBootTest
 class TliasWebManagementApplicationTests {
+
+
 
     @Test
     void contextLoads() {
@@ -41,8 +47,27 @@ class TliasWebManagementApplicationTests {
                 .setSigningKey("itheima")
                 .parseClaimsJws("eyJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoidG9tIiwiaWQiOjEsImV4cCI6MTcxNDE0MzI3MH0.NZDGxGGvnc1F3shMfvzmFncNqNHq6rayJQF3jp8h7XI")
                 .getBody();
-
+ 
         System.out.println("res: "+claims.toString());
     }
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    public void TestGetBean(){
+        // 根据 bean 的名称来获取
+        DeptController bean1 = (DeptController) applicationContext.getBean("deptController");
+        System.out.println(bean1);
+        // 根据bean 的类型来获取
+        DeptController bean2 = applicationContext.getBean(DeptController.class);
+        System.out.println(bean2);
+
+        // 根据 bean 的名称和类型来获取
+        DeptController bean3 = applicationContext.getBean("deptController", DeptController.class);
+        System.out.println(bean3);
+
+    }
+
 
 }
