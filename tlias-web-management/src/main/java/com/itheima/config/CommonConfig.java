bdfb42499f08f5a278b93a987d9830191c9dd4a6
Haji <835510106@qq.com>
2024-04-29 19:50:05 +0800

    Create CommonConfig.java

---
+++ tlias-web-management/src/main/java/com/itheima/config/CommonConfig.java
@@ -0,0 +1,13 @@
+package com.itheima.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+
+@Configurable
+public class CommonConfig {
+    // 声明第三方bean
+//    @Bean
+//    public SAXReader saxReader{
+//
+//    }
+}
